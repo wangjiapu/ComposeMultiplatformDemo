ba307e41d1258e1a5caa84ed744353a033178f6d
pupu <857484057@qq.com>
2025-12-10 08:11:52 +0800

    协程上下文/拦截器

--- composeApp/src/commonMain/kotlin/org/examp/project/test1/CoroutineTest.kt
+++ composeApp/src/commonMain/kotlin/org/examp/project/test1/CoroutineTest.kt
@@ -1,12 +1,15 @@
 package org.examp.project.test1
 
 import io.github.aakira.napier.Napier
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.delay
 import kotlin.coroutines.AbstractCoroutineContextElement
 import kotlin.coroutines.Continuation
+import kotlin.coroutines.ContinuationInterceptor
 import kotlin.coroutines.CoroutineContext
 import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.coroutines.RestrictsSuspension
+import kotlin.coroutines.coroutineContext
 import kotlin.coroutines.createCoroutine
 import kotlin.coroutines.resume
 import kotlin.coroutines.startCoroutine
@@ -25,6 +28,20 @@ class ProducerScope<T> {
 @RestrictsSuspension
 class ProducerScope2<T> {
     suspend fun produce(value: T) {
+    }
+}
+
+class MyCoroutineName(val name: String) : AbstractCoroutineContextElement(key = Key) {
+    companion object Key : CoroutineContext.Key<MyCoroutineName>
+}
+
+class MyCoroutineExceptionHandler(val onErrorAction: (Throwable) -> Unit) :
+    AbstractCoroutineContextElement(key = Key) {
+    companion object Key : CoroutineContext.Key<MyCoroutineExceptionHandler>
+
+    fun onError(error: Throwable) {
+        error.printStackTrace()
+        onErrorAction(error)
     }
 }
 
@@ -60,6 +77,7 @@ class CoroutineTest {
         })
     }
 
+    // 协程作用域
     fun callLaunchCoroutine() {
         launchCoroutine(ProducerScope<Int>()) {
             Napier.e(message = "start")
@@ -85,18 +103,57 @@ class CoroutineTest {
         continuation.resume(100)
     }
 
+    // 协程上下文
+    fun coroutineContextTest() {
+        val continuation = suspend {
+            5
+        }.createCoroutine(object : Continuation<Int> {
+
+            override val context: CoroutineContext
+                get() = EmptyCoroutineContext + MyCoroutineName("pupu") + MyCoroutineExceptionHandler { e ->
+                    Napier.e { e.printStackTrace().toString() }
+                }
+
+
+            override fun resumeWith(result: Result<Int>) {
+                result.onFailure {
+                    context[MyCoroutineExceptionHandler]?.onError(it)
+                }
+            }
+        })
+
+    }
+
+    // 协程拦截器
+    fun coroutineIntercept() {
+        val continuation = suspend {
+
+            4
+        }.startCoroutine(object : Continuation<Int> {
+            override val context: CoroutineContext
+                get() = LogInterceptor()
+
+            override fun resumeWith(result: Result<Int>) {
+                ////
+            }
+        })
+    }
 }
 
-class CoroutineName(val name: String) : AbstractCoroutineContextElement(key = Key) {
-    companion object Key : CoroutineContext.Key<CoroutineName>
-}
 
-class CoroutineExceptionHandler(val onErrorAction: (Throwable) -> Unit) :
-    AbstractCoroutineContextElement(key = Key) {
-    companion object Key : CoroutineContext.Key<CoroutineExceptionHandler>
+class LogInterceptor : ContinuationInterceptor {
+    override val key: CoroutineContext.Key<*>
+        get() = ContinuationInterceptor // 拦截器的key是一个固定实现
 
-    fun onError(error: Throwable) {
-        error.printStackTrace()
-        onErrorAction(error)
+    override fun <T> interceptContinuation(continuation: Continuation<T>): Continuation<T> {
+        return LogContinuation(continuation)
+    }
+
+    class LogContinuation<T>(private val continuation: Continuation<T>) :
+        Continuation<T> by continuation {
+        override fun resumeWith(result: Result<T>) {
+            Napier.e(message = "执行log")
+            continuation.resumeWith(result)
+        }
     }
 }
