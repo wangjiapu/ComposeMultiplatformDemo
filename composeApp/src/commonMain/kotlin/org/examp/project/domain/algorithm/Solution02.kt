37c824ae93ccf0acee59e530c33baeac741e8a5f
pujiawang <pujiawang@kkworld.com>
2026-02-26 11:45:48 +0800

    合并二叉树

--- composeApp/src/commonMain/kotlin/org/examp/project/domain/algorithm/Solution02.kt
+++ composeApp/src/commonMain/kotlin/org/examp/project/domain/algorithm/Solution02.kt
@@ -167,6 +167,23 @@ class Solution02 {
         return true
     }
 
+    /**
+     * 合并二叉树
+     */
+    fun mergeTrees(t1: TreeNode?, t2: TreeNode?): TreeNode? {
+        if (t1 == null) {
+            return t2
+        }
+        if (t2 == null) {
+            return t1
+        }
+        val newRoot = TreeNode((t1.`val`) + (t2.`val`))
+        newRoot.left = mergeTrees(t1.left, t2.left)
+        newRoot.right = mergeTrees(t1.right, t2.right)
+        return newRoot
+    }
+
+
     private fun traversal3(root: TreeNode?, result: MutableList<Int>) {
         if (root == null) {
             return
