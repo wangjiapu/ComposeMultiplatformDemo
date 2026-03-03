b49bb99b81bac54244d9582ced1fc0be3e20739b
pujiawang <pujiawang@kkworld.com>
2026-03-03 11:21:41 +0800

    数组中出现次数超过一半的数字

--- composeApp/src/commonMain/kotlin/org/examp/project/domain/algorithm/Hash.kt
+++ composeApp/src/commonMain/kotlin/org/examp/project/domain/algorithm/Hash.kt
@@ -1,6 +1,9 @@
 package org.examp.project.domain.algorithm
 
 class Hash {
+    /**
+     * 两数之和
+     */
     fun twoSum(numbers: IntArray, target: Int): IntArray {
         val hashMap = HashMap<Int, Int>(numbers.size)
         numbers.forEachIndexed { index, value ->
@@ -11,5 +14,25 @@ class Hash {
             }
         }
         return intArrayOf()
+    }
+
+    /**
+     * 数组中出现次数超过一半的数字
+     */
+    fun moreThanHalfNum(numbers: IntArray): Int {
+        val hashMap = HashMap<Int, Int>()
+        numbers.forEach {
+            if (hashMap.containsKey(it)) {
+                hashMap[it] = hashMap[it]?.plus(1) ?: 0
+            } else {
+                hashMap[it] = 1
+            }
+            hashMap[it]?.let { it1 ->
+                if (it1 > numbers.size / 2) {
+                    return it
+                }
+            }
+        }
+        return 0
     }
 }
