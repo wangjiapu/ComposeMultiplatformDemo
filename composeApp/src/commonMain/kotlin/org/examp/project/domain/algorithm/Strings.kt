07ae053304e981ee294f7f2b74b27d98ac003831
pujiawang <pujiawang@kkworld.com>
2026-03-04 12:18:52 +0800

    字符串变形

---
+++ composeApp/src/commonMain/kotlin/org/examp/project/domain/algorithm/Strings.kt
@@ -0,0 +1,42 @@
+package org.examp.project.domain.algorithm
+
+class Strings {
+    /**
+     *字符串变形
+     */
+    fun trans(s: String, n: Int): String {
+        if (n == 0) {
+            return s
+        }
+        val swapped = s.map {
+            if (it.isUpperCase()) it.lowercase() else it.uppercase()
+        }.joinToString("")
+
+        val stack = ArrayDeque<String>()
+        val strs = swapped.split(" ")
+        strs.forEach {
+            stack.addFirst(it)
+        }
+        var sb = StringBuilder()
+        while (stack.isNotEmpty()) {
+            sb.append(stack.removeFirst())
+            sb.append(" ")
+        }
+        return sb.toString().trim()
+    }
+
+    private fun reverseStr(s: String): String {
+        var chars = s.toCharArray()
+        var left = 0
+        var right = s.length - 1
+        while (left < right) {
+            var temp = chars[left]
+            chars[left] = chars[right]
+            chars[right] = temp
+            left++
+            right--
+        }
+        return chars.concatToString()
+    }
+
+}
